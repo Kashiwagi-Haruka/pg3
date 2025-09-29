7b55a37aca10cfe895034e72685ff2a4d7e76552
Kashiwagi-Haruka <k024g0020@g.neec.ac.jp>
2025-09-29 12:36:55 +0900

    01-01終わり

--- 01-01/main.cpp
+++ 01-01/main.cpp
@@ -5,19 +5,59 @@ using namespace std;
 
 int main(){
 
-	list<int> stationList;
-	for (int i = 0; i < 30; i++) {
-		stationList.push_back(i+1);
-	}
+	list<const char*> stationList = {"Tokyo",  "Kanda",     "Akihabara", "Okachimachi",  "Ueno",       "Uguisudani",   "Nippori",  "Tabata",   "Komagome", "Sugamo",
+	                                  "Otsuka", "Ikebukuro", "Mejiro",    "Takadanobaba", "Shin-Okubo", "Shinjuku",     "Yoyogi",    "Harajuku", "Shibuya",  "Ebisu",
+	                                  "Meguro", "Gotanda",   "Osaki",     "Shinagawa" ,  "Tamachi",    "Hamamatsucho", "Shimbashi", "Yurakucho"};
 
 
-
-	list<int>::iterator itr;
+	list<const char*>::iterator itr;
 	itr = stationList.begin();
 	
-	for (int i = 0; i < stationList.size(); i++) {
-		printf("%d\n", *itr);
+
+	printf("山手線の駅名一覧\n");
+	printf("----------------\n");
+	printf("1970年版\n\n");
+
+	for (int i = 0; itr != stationList.end(); i++) {
+
+		
+		printf("JY%d : ", i + 1);
+		printf("%s\n", *itr);
 		itr++;
+	}
+
+	printf("----------------\n");
+	printf("2019年版\n\n");
+	itr = stationList.begin();
+	for (int i = 0; itr != stationList.end(); i++) {
+		
+		printf("JY%d : ", i + 1);
+		printf("%s\n", *itr);
+
+		if (*itr == "Nippori") {
+			itr = stationList.insert(++itr, "Nishi-Nippori"); 
+		} else {
+			itr++;
+		}
+
+	
+	}
+
+	printf("----------------\n");
+	printf("2022年版\n\n");
+	itr = stationList.begin();
+	for (int i = 0; itr != stationList.end(); i++) {
+		printf("JY%d : ", i + 1);
+		printf("%s\n", *itr);
+
+		if (*itr == "Shinagawa") {
+			itr = stationList.insert(++itr, "Takanawa Gateway");
+		} else {
+			itr++;
+		
+		}
+
+		
 	}
 
 
