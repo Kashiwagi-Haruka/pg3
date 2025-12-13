b46b2575bd3672dac75417da67d726822ca5dd0d
Kashiwagi-Haruka <k024g0020@g.neec.ac.jp>
2025-12-13 12:20:11 +0900

    ほぞｎ

--- 01-01/main.cpp
+++ 01-01/main.cpp
@@ -1,52 +1,54 @@
+#include <cstring>
 #include <list>
 #include <stdio.h>
 using namespace std;
+
 int main() {
 
 	list<const char*> stationList = {"Tokyo",  "Kanda",     "Akihabara", "Okachimachi",  "Ueno",       "Uguisudani",   "Nippori",   "Tabata",   "Komagome", "Sugamo",
 	                                 "Otsuka", "Ikebukuro", "Mejiro",    "Takadanobaba", "Shin-Okubo", "Shinjuku",     "Yoyogi",    "Harajuku", "Shibuya",  "Ebisu",
 	                                 "Meguro", "Gotanda",   "Osaki",     "Shinagawa",    "Tamachi",    "Hamamatsucho", "Shimbashi", "Yurakucho"};
 
-	list<const char*>::iterator itr;
-	itr = stationList.begin();
+	list<const char*>::iterator itr = stationList.begin();
 
 	printf("山手線の駅名一覧\n");
 	printf("----------------\n");
 	printf("1970年版\n\n");
 
 	for (int i = 0; itr != stationList.end(); i++) {
-
-		printf("JY%d : ", i + 1);
-		printf("%s\n", *itr);
-		itr++;
+		printf("JY%d : %s\n", i + 1, *itr);
+		++itr;
 	}
 
 	printf("----------------\n");
 	printf("2019年版\n\n");
+
 	itr = stationList.begin();
 	for (int i = 0; itr != stationList.end(); i++) {
 
-		printf("JY%d : ", i + 1);
-		printf("%s\n", *itr);
+		printf("JY%d : %s\n", i + 1, *itr);
 
-		if (*itr == "Nippori") {
-			itr = stationList.insert(++itr, "Nishi-Nippori");
+		if (strcmp(*itr, "Nippori") == 0) {
+			++itr;
+			itr = stationList.insert(itr, "Nishi-Nippori");
 		} else {
-			itr++;
+			++itr;
 		}
 	}
 
 	printf("----------------\n");
 	printf("2022年版\n\n");
+
 	itr = stationList.begin();
 	for (int i = 0; itr != stationList.end(); i++) {
-		printf("JY%d : ", i + 1);
-		printf("%s\n", *itr);
 
-		if (*itr == "Shinagawa") {
-			itr = stationList.insert(++itr, "Takanawa Gateway");
+		printf("JY%d : %s\n", i + 1, *itr);
+
+		if (strcmp(*itr, "Shinagawa") == 0) {
+			++itr;
+			itr = stationList.insert(itr, "Takanawa Gateway");
 		} else {
-			itr++;
+			++itr;
 		}
 	}
 
