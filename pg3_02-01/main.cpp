2c15e4f2868b66273db0964ff2c1fcddb62e58d4
Kashiwagi-Haruka <k024g0020@g.neec.ac.jp>
2025-12-24 17:01:15 +0900

    a

--- pg3_02-01/main.cpp
+++ pg3_02-01/main.cpp
@@ -1,25 +1,25 @@
 #define NOMINMAX
-#include <stdio.h>
 #include <algorithm>
-template <typename Type>
+#include <stdio.h>
 
-Type min(Type a, Type b){
-	return static_cast<Type>(std::min(a,b));
+template<typename Type>
+
+Type min(Type a, Type b) {
+	return static_cast<Type>(std::min(a, b)); 
 }
 
-int main(){
+int main() {
 
-	int a = 5;
-	int b = 9;
-	float c = 3;
-	float d = 7;
-	double e = 10;
-	double f = 4;
+	int a = 5; 
+	int b = 9; 
+	float c = 3; 
+	float d = 7; 
+	double e = 10; 
+	double f = 4; 
 
+	printf("%d\n", min<int>(a, b)); 
+	printf("%f\n", min<float>(c, d)); 
+	printf("%lf\n", min<double>(e, f)); 
 
-	printf("%d\n", min<int>(a, b));
-	printf("%f\n",min<float>(c,d));
-	printf("%lf\n", min<double>(e, f));
-
-	return 0;
+	return 0; 
 }
