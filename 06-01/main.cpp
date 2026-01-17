2ffb650138486f5768b4cc4f27f839f1e2aa3ea1
Kashiwagi-Haruka <k024g0020@g.neec.ac.jp>
2026-01-17 23:20:50 +0900

    a

--- 06-01/main.cpp
+++ 06-01/main.cpp
@@ -1,7 +1,29 @@
-#include <stdio.h>
-int main(){
+#include <condition_variable>
+#include <iostream>
+#include <mutex>
+#include <thread>
 
+int main() {
+	std::condition_variable cv;
+	std::mutex mutex;
+	int turn = 1;
 
+	auto run = [&](int id) {
+		std::unique_lock<std::mutex> lock(mutex);
+		cv.wait(lock, [&] { return turn == id; });
+		std::cout << "thread " << id << std::endl;
+		++turn;
+		lock.unlock();
+		cv.notify_all();
+	};
+
+	std::thread t1(run, 1);
+	std::thread t2(run, 2);
+	std::thread t3(run, 3);
+
+	t1.join();
+	t2.join();
+	t3.join();
 
 	return 0;
 }
